0f6cd7af9bd01df76a5a28209866625483354287
Santhosh J <ssjdexucyche@gmail.com>
2023-06-09 10:37:42 +0530

    added email validation and form submit

--- src/components/EmailForm/EmailForm.jsx
+++ src/components/EmailForm/EmailForm.jsx
@@ -2,17 +2,46 @@ import React, { useState } from "react";
 import styles from "./EmailForm.module.scss";
 const EmailForm = () => {
   const [input, setInput] = useState("");
+  const [error, setError] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!input) {
+      setError(true);
+      return;
+    }
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    // check if email is valid
+    if (!mailFormat.test(input)) {
+      setError(true);
+    } else {
+      window.location.href = "https://app.loch.one/welcome";
+    }
+  };
   return (
     <div className={styles["container"]}>
       <h1>Sign up for exclusive access.</h1>
-      <input
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder="Your email address"
-        className={`custom-input ${styles["input"]}`}
-      />
-      <button className={`custom-button-xl`}>Get started</button>
-      <p>You’ll receive an email with an invite link to join.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={input}
+          onChange={(e) => {
+            setError(false);
+            setInput(e.target.value);
+          }}
+          placeholder="Your email address"
+          className={`custom-input ${styles["input"]} ${
+            error && `error-border`
+          }`}
+        />
+        {error && (
+          <p className="error-text">Please enter a valid email address.</p>
+        )}
+      </form>
+      <button className={`custom-button-xl`} type="submit">
+        Get started
+      </button>
+      <p className={styles["form-info"]}>
+        You’ll receive an email with an invite link to join.
+      </p>
     </div>
   );
 };
